56e631f0199ac12e34b39e0eaf2344cba287adb4
bagashanantoputra <danandjaya123@gmail.com>
2023-10-09 17:45:02 +0700

    Slider

--- src/customer/components/HomeSectionCard/MainSectionData.js
+++ src/customer/components/HomeSectionCard/MainSectionData.js
@@ -35,4 +35,13 @@ export const sectionData = [
         price: '$35',
         color: 'Iso Dots',
     },
+    {
+        id: 5,
+        name: 'Basic Tee',
+        href: '#',
+        imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg',
+        imageAlt: "Front of men's Basic Tee in black.",
+        price: '$35',
+        color: 'Black',
+    },
 ]
